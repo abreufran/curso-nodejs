8a64268d77e9e19ed67c4401a65f281c7f3049cb
Francisco Abreu <francisco.abreu@externos-cl.cencosud.com>
2021-05-19 14:54:55 -0400

    servidor http

---
+++ test-http.js
@@ -0,0 +1,13 @@
+const http = require('http');
+
+const handlerServer = function(req, res) {
+    res.writeHead(200, {'Content-type': 'text/html'});
+    res.write('<h1>Hola Mundo desde NodeJs</h1>');
+    res.end();
+}
+
+const server = http.createServer(handlerServer);
+
+server.listen(3000, function() {
+    console.log('Servidor en puerto 3000');
+});    
