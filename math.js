4dabaafee7cbf534133becadfc6dd36756ae8ff4
Francisco Abreu <francisco.abreu@externos-cl.cencosud.com>
2021-05-19 13:57:18 -0400

    fuentes iniciales

---
+++ math.js
@@ -0,0 +1,39 @@
+const Math = {};
+
+function add(x1, x2) {
+    return x1 + x2;
+}
+
+function substract(x1, x2) {
+    return x1 - x2;
+}
+
+function multiply(x1, x2) {
+    return x1 * x2;
+}
+
+function divide(x1, x2) {
+    if(x2 == 0) {
+        console.log("No se puede dividir por cero.")
+    }
+    return x1 / x2;
+}
+
+function hello(name){
+    console.log("Hola ", name);
+}
+
+Math.add = add;
+Math.substract = substract;
+Math.multiply = multiply;
+Math.divide = divide;
+
+module.exports = Math;
+
+
+/*
+exports.add = add;
+exports.substract = substract;
+exports.multiply = multiply;
+exports.divide = divide;
+*/
