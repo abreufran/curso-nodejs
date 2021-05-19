5953ca63c805a83125062d2c6521c9db66b2fc15
Francisco Abreu <francisco.abreu@externos-cl.cencosud.com>
2021-05-19 15:01:53 -0400

    se agrego el modulo de color

--- test-http.js
+++ test-http.js
@@ -1,5 +1,7 @@
 const http = require('http');
 
+const color = require('colors');
+
 const handlerServer = function(req, res) {
     res.writeHead(200, {'Content-type': 'text/html'});
     res.write('<h1>Hola Mundo desde NodeJs</h1>');
@@ -9,5 +11,5 @@ const handlerServer = function(req, res) {
 const server = http.createServer(handlerServer);
 
 server.listen(3000, function() {
-    console.log('Servidor en puerto 3000');
+    console.log('Servidor en puerto 3000'.green);
 });    
